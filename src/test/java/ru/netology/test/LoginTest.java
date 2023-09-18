0c9cb6609a9af64d15f73cd9bdf106977c761ce4
daishan <olegkajlev@gmail.com>
2023-09-18 11:55:22 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,34 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class LoginTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() { cleanAuthCodes(); }
+
+    @AfterAll
+    static void tearDownAll() { cleanDatabase(); }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void  shouldSuccessfulLogin() {
+        var autInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(autInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
